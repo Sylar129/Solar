ead56d1ab4439eb1d96cb66f8e6f74ad55e5a74b
Sylar <710288202@qq.com>
2022-03-16 20:43:23 +0800

    Basic ref-counting system to terminate glfw

--- Solar/src/Platform/Windows/WindowsWindow.cpp
+++ Solar/src/Platform/Windows/WindowsWindow.cpp
@@ -9,7 +9,7 @@
 
 namespace Solar {
 
-    static bool s_GLFWInitialized = false;
+    static uint8_t s_GLFWWindowCount = 0;
 
     static void GLFWErrorCallback(int error, const char* description) {
         SOLAR_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
@@ -52,11 +52,11 @@ namespace Solar {
 
         SOLAR_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
 
-        if (!s_GLFWInitialized) {
+        if (s_GLFWWindowCount == 0) {
+            SOLAR_CORE_INFO("Initializing GLFW");
             int success = glfwInit();
-            SOLAR_ASSERT(success, "Could not intiliaze GLFW!");
+            SOLAR_CORE_ASSERT(success, "Could not intiliaze GLFW!");
             glfwSetErrorCallback(GLFWErrorCallback);
-            s_GLFWInitialized = true;
         }
 
         m_Window = glfwCreateWindow((int)props.Width, props.Height, props.Title.c_str(), nullptr, nullptr);
@@ -142,5 +142,10 @@ namespace Solar {
 
     void WindowsWindow::Shutdown() {
         glfwDestroyWindow(m_Window);
+        s_GLFWWindowCount--;
+        if (s_GLFWWindowCount == 0) {
+            SOLAR_CORE_INFO("Terminating GLFW");
+            glfwTerminate();
+        }
     }
 }
