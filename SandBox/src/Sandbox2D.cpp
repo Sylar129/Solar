1bac2368b5055c1e5771bb6d90a8df20b38b2301
Sylar <710288202@qq.com>
2022-03-16 21:10:35 +0800

    Add Renderer2D Transform

--- SandBox/src/Sandbox2D.cpp
+++ SandBox/src/Sandbox2D.cpp
@@ -30,16 +30,10 @@ void Sandbox2D::OnUpdate(Solar::TimeStep& ts) {
 
     Solar::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-    Solar::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+    Solar::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.5f, 0.5f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+    Solar::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.5f }, { 0.3f, 0.8f, 0.2f, 1.0f });
 
     Solar::Renderer2D::EndScene();
-    //static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
-
-    //std::dynamic_pointer_cast<Solar::OpenGLShader>(m_FlatColorShader)->Bind();
-    //std::dynamic_pointer_cast<Solar::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
-
-    //Solar::Renderer::Submit(m_FlatColorShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
-
 }
 
 void Sandbox2D::OnImGuiRender() {
