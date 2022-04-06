c282703905144c55ab2385d261c35bf1966e02a6
Sylar <710288202@qq.com>
2022-04-06 23:36:46 +0800

    Batch Rendering

--- Solar/src/Platform/OpenGL/OpenGLBuffer.cpp
+++ Solar/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -5,6 +5,14 @@
 
 namespace Solar {
     // OpenGLVertexBuffer
+    OpenGLVertexBuffer::OpenGLVertexBuffer(uint32_t size) {
+        SOLAR_PROFILE_FUNCTION();
+
+        glCreateBuffers(1, &m_RendererID);
+        glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+        glBufferData(GL_ARRAY_BUFFER, size, nullptr, GL_DYNAMIC_DRAW);
+    }
+
     OpenGLVertexBuffer::OpenGLVertexBuffer(float* vertices, uint32_t size) {
         SOLAR_PROFILE_FUNCTION();
 
@@ -29,6 +37,11 @@ namespace Solar {
         SOLAR_PROFILE_FUNCTION();
 
         glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    void OpenGLVertexBuffer::SetData(const void* data, uint32_t size) {
+        glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, size, data);
     }
 
     const BufferLayout& OpenGLVertexBuffer::GetLayout() const {
--- Solar/src/Solar/Renderer/Buffer.cpp
+++ Solar/src/Solar/Renderer/Buffer.cpp
@@ -5,6 +5,17 @@
 #include "Platform/OpenGL/OpenGLBuffer.h"
 
 namespace Solar {
+    Ref<VertexBuffer> VertexBuffer::Create(uint32_t size) {
+        switch (Renderer::GetAPI()) {
+        case RendererAPI::API::None:
+            SOLAR_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+            return nullptr;
+        case RendererAPI::API::OpenGL:
+            return CreateRef<OpenGLVertexBuffer>(size);
+        }
+        SOLAR_CORE_ASSERT(false, "Unknown RendererAPI");
+        return nullptr;
+    }
     Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size) {
         switch (Renderer::GetAPI()) {
             case RendererAPI::API::None:
