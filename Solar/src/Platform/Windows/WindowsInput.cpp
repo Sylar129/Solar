61c2f3c7270b0c8dc4361ab88d0aae81039274e1
Sylar <710288202@qq.com>
2022-04-23 22:35:08 +0800

    Refactor Input

--- Solar/src/Platform/Windows/WindowsInput.cpp
+++ Solar/src/Platform/Windows/WindowsInput.cpp
@@ -1,51 +1,35 @@
 #include "solpch.h"
-#include "WindowsInput.h"
+#include "Solar/Core/Input.h"
 
 #include "Solar/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace Solar {
-    Scope<Input> Input::s_Instance = CreateScope<WindowsInput>();
-
-    bool WindowsInput::IsKeyPressedImpl(KeyCode keycode) {
+    bool Input::IsKeyPressed(KeyCode keycode) {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetKey(window, static_cast<uint16_t>(keycode));
 
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
-    bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button) {
+    bool Input::IsMouseButtonPressed(MouseCode button) {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetMouseButton(window, static_cast<uint16_t>(button));
 
         return state == GLFW_PRESS;
     }
-    std::pair<float, float> WindowsInput::GetMousePositionImpl() {
+    std::pair<float, float> Input::GetMousePosition() {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         double xpos, ypos;
         glfwGetCursorPos(window, &xpos, &ypos);
 
         return { (float)xpos, (float)ypos };
     }
-    float WindowsInput::GetMouseXImpl() {
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-        double xpos, ypos;
-        glfwGetCursorPos(window, &xpos, &ypos);
-
-        return (float)xpos;
-
-        // C++ 17
-        //auto [x, y] = GetMousePositionImpl();
-        //return x;
+    float Input::GetMouseX() {
+        auto [x, y] = GetMousePosition();
+        return x;
     }
-    float WindowsInput::GetMouseYImpl() {
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-        double xpos, ypos;
-        glfwGetCursorPos(window, &xpos, &ypos);
-
-        return (float)ypos;
-
-        // C++ 17
-        //auto [x, y] = GetMousePositionImpl();
-        //return y;
+    float Input::GetMouseY() {
+        auto [x, y] = GetMousePosition();
+        return y;
     }
 }
--- Solar/src/Solar/Core/Input.h
+++ Solar/src/Solar/Core/Input.h
@@ -6,39 +6,10 @@
 namespace Solar {
     class Input {
     public:
-        virtual ~Input() = default;
-
-        Input(const Input&) = delete;
-        Input& operator=(const Input&) = delete;
-
-        inline static bool IsKeyPressed(KeyCode keycode) {
-            return s_Instance->IsKeyPressedImpl(keycode);
-        }
-
-        inline static bool IsMouseButtonPressed(MouseCode button) {
-            return s_Instance->IsMouseButtonPressedImpl(button);
-        }
-
-        inline static std::pair<float, float> GetMousePosition() {
-            return s_Instance->GetMousePositionImpl();
-        }
-
-        inline static float GetMouseX() {
-            return s_Instance->GetMouseXImpl();
-        }
-
-        inline static float GetMouseY() {
-            return s_Instance->GetMouseYImpl();
-        }
-    protected:
-        Input() = default;
-        virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
-
-        virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-        virtual std::pair<float, float> GetMousePositionImpl() = 0;
-        virtual float GetMouseXImpl() = 0;
-        virtual float GetMouseYImpl() = 0;
-    private:
-        static Scope<Input> s_Instance;
+        static bool IsKeyPressed(KeyCode keycode);
+        static bool IsMouseButtonPressed(MouseCode button);
+        static std::pair<float, float> GetMousePosition();
+        static float GetMouseX();
+        static float GetMouseY();
     };
 }
