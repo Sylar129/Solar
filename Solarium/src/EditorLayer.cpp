ef18e2dfb79cbfa6f75d16689539580dc3420839
Sylar <710288202@qq.com>
2022-05-23 23:35:04 +0800

    Add SpriteRenderer Component UI

--- Solarium/src/EditorLayer.cpp
+++ Solarium/src/EditorLayer.cpp
@@ -29,10 +29,10 @@ namespace Solar {
 
         m_SquareEntity = square;
 
-        m_CameraEntity = m_ActiveScene->CreateEntity("Camera Entity");
+        m_CameraEntity = m_ActiveScene->CreateEntity("Camera A");
         m_CameraEntity.AddComponent<CameraComponent>();
 
-        m_SecondCamera = m_ActiveScene->CreateEntity("Second Camera Entity");
+        m_SecondCamera = m_ActiveScene->CreateEntity("Camera B");
         auto& cc =  m_SecondCamera.AddComponent<CameraComponent>();
         cc.Primary = false;
 
@@ -173,37 +173,13 @@ namespace Solar {
         // Panels
         m_SceneHierarchyPanel.OnImGuiRender();
 
-        ImGui::Begin("Settings");
+        ImGui::Begin("Stats");
         auto stats = Renderer2D::GetStats();
         ImGui::Text("Renderer2D stats:");
         ImGui::Text("Draw calls: %d", stats.DrawCalls);
         ImGui::Text("Quads: %d", stats.QuadCount);
         ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
         ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-
-        if (m_SquareEntity) {
-            ImGui::Separator();
-            ImGui::Text("%s", m_SquareEntity.GetComponent<TagComponent>().Tag.c_str());
-
-            auto& squareColor = m_SquareEntity.GetComponent<SpriteRendererComponent>().Color;
-            ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
-            ImGui::Separator();
-        }
-
-        ImGui::DragFloat3("Camera Transform", glm::value_ptr(m_CameraEntity.GetComponent<TransformComponent>().Transform[3]));
-        if (ImGui::Checkbox("Camera A", &m_PrimaryCamera)) {
-            m_CameraEntity.GetComponent<CameraComponent>().Primary = m_PrimaryCamera;
-            m_SecondCamera.GetComponent<CameraComponent>().Primary = !m_PrimaryCamera;
-        }
-
-        {
-            auto& camera = m_SecondCamera.GetComponent<CameraComponent>().Camera;
-            float orthoSize = camera.GetOrthographicSize();
-            if (ImGui::DragFloat("Second Camera Ortho Size", &orthoSize)) {
-                camera.SetOrthographicSize(orthoSize);
-            }
-        }
-
         ImGui::End();
 
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0,0 });
