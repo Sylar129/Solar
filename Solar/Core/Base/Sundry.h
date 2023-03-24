d38b201ff1df5c866549a124f9980672bbe01894
Sylar129 <58684471+Sylar129@users.noreply.github.com>
2023-03-24 22:01:12 +0800

    feat: replace macro with constexpr template

--- Solar/Core/Base/Sundry.h
+++ Solar/Core/Base/Sundry.h
@@ -34,14 +34,22 @@
 #define SOLAR_CORE_ASSERT(x, ...)
 #endif
 
-#define BIT(x) (1 << x)
-
 #define SOLAR_BIND_EVENT_FN(fn)                                                \
     [this](auto&&... args) -> decltype(auto) {                                 \
         return this->fn(std::forward<decltype(args)>(args)...);                \
     }
 
 namespace Solar {
+
+template <bool SHIFT = true>
+constexpr auto BIT(int n)
+{
+    if constexpr (SHIFT) {
+        return 1 << n;
+    } else {
+        return n;
+    }
+}
 
 template <typename T>
 using Scope = std::unique_ptr<T>;
