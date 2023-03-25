f668db016f050fbec9192b22dc8f6fc830de513a
Sylar129 <710288202@qq.com>
2023-03-25 20:56:44 +0800

    chore: update .vcxproj file

--- Solar/Platform/OpenGL/OpenGLContext.cpp
+++ Solar/Platform/OpenGL/OpenGLContext.cpp
@@ -33,7 +33,7 @@ void OpenGLContext::Init()
 
     SOLAR_CORE_ASSERT(versionMajor > 4 ||
                           (versionMajor == 4 && versionMinor >= 5),
-                      "Hazel requires at least OpenGL version 4.5!");
+                      "Solar requires at least OpenGL version 4.5!");
 #endif
 }
 
