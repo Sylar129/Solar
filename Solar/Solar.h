a8f5e9078130f1f7fd88a355c7d0bd0c6cd05a93
Sylar129 <710288202@qq.com>
2023-03-26 10:42:13 +0800

    chore: sort the include files

--- Solar/Solar.h
+++ Solar/Solar.h
@@ -4,17 +4,17 @@
 #include "Core/Base/Application.h"
 #include "Core/Base/Input.h"
 #include "Core/Base/KeyCodes.h"
-#include "Core/Base/MouseCodes.h"
 #include "Core/Base/Layer.h"
 #include "Core/Base/Log.h"
+#include "Core/Base/MouseCodes.h"
 #include "Core/Base/Timestep.h"
 
 #include "Core/ImGui/ImGuiLayer.h"
 
-#include "Core/Scene/Scene.h"
-#include "Core/Scene/Entity.h"
-#include "Core/Scene/ScriptableEntity.h"
 #include "Core/Scene/Components.h"
+#include "Core/Scene/Entity.h"
+#include "Core/Scene/Scene.h"
+#include "Core/Scene/ScriptableEntity.h"
 
 // -------------- --Renderer---------------
 #include "Core/Renderer/Renderer.h"
@@ -22,10 +22,10 @@
 #include "Core/Renderer/RenderCommand.h"
 
 #include "Core/Renderer/Buffer.h"
-#include "Core/Renderer/Shader.h"
 #include "Core/Renderer/Framebuffer.h"
-#include "Core/Renderer/Texture.h"
+#include "Core/Renderer/Shader.h"
 #include "Core/Renderer/SubTexture2D.h"
+#include "Core/Renderer/Texture.h"
 #include "Core/Renderer/VertexArray.h"
 
 #include "Core/Renderer/OrthographicCamera.h"
