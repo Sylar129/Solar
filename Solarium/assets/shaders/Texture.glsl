5ef84f7d8b71a7770831694a4924a9b681983519
Sylar129 <710288202@qq.com>
2023-03-26 16:03:41 +0800

    feat: update Texture.glsl

--- Solarium/assets/shaders/Texture.glsl
+++ Solarium/assets/shaders/Texture.glsl
@@ -1,7 +1,7 @@
 // Texture Shader
 
 #type vertex
-#version 330 core
+#version 450 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
@@ -10,39 +10,49 @@ layout(location = 3) in float a_TexIndex;
 layout(location = 4) in float a_TilingFactor;
 layout(location = 5) in int a_EntityID;
 
-uniform mat4 u_ViewProjection;
+layout(std140, binding = 0) uniform Camera {
+    mat4 u_ViewProjection;
+};
 
-out vec4 v_Color;
-out vec2 v_TexCoord;
-out float v_TexIndex;
-out float v_TilingFactor;
-flat out int v_EntityID;
+struct VertexOutput {
+    vec4 Color;
+    vec2 TexCoord;
+    float TexIndex;
+    float TilingFactor;
+};
+
+layout (location = 0) out VertexOutput Output;
+layout (location = 4) out flat int v_EntityID;
 
 void main() {
-    v_Color = a_Color;
-    v_TexCoord = a_TexCoord;
-    v_TexIndex = a_TexIndex;
-    v_TilingFactor = a_TilingFactor;
+    Output.Color = a_Color;
+    Output.TexCoord = a_TexCoord;
+    Output.TexIndex = a_TexIndex;
+    Output.TilingFactor = a_TilingFactor;
     v_EntityID = a_EntityID;
     gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }
 
 
 #type fragment
-#version 330 core
+#version 450 core
 
 layout(location = 0) out vec4 color;
 layout(location = 1) out int color2;
 
-in vec4 v_Color;
-in vec2 v_TexCoord;
-in float v_TexIndex;
-in float v_TilingFactor;
-flat in int v_EntityID;
+struct VertexOutput {
+    vec4 Color;
+    vec2 TexCoord;
+    float TexIndex;
+    float TilingFactor;
+};
 
-uniform sampler2D u_Texture[32];
+layout(location = 0) in VertexOutput Input;
+layout(location = 4) in flat int v_EntityID;
+
+layout(binding = 0) uniform sampler2D u_Texture[32];
 
 void main() {
-    color = texture(u_Texture[int(v_TexIndex)], v_TexCoord * v_TilingFactor) * v_Color;
+    color = texture(u_Texture[int(Input.TexIndex)], Input.TexCoord * Input.TilingFactor) * Input.Color;
     color2 = v_EntityID;
 }
